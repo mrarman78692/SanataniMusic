e38f61aa5d61e7a1b89e1f93a68db701913fb2f7
Aarumi Bots <itzshivanshdevs@gmail.com>
2025-06-04 18:23:09 +0530

    Update start.py

--- AarumiMusic/plugins/bot/start.py
+++ AarumiMusic/plugins/bot/start.py
@@ -1,89 +1,99 @@
 import asyncio
+import random
 import time
-import random 
 from pyrogram import filters
-from pyrogram.enums import ChatType, ParseMode
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from AarumiMusic import app
 from AarumiMusic.misc import _boot_
 from AarumiMusic.plugins.sudo.sudoers import sudoers_list
+from AarumiMusic.utils import bot_sys_stats
 from AarumiMusic.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
     get_lang,
+    get_served_chats,
+    get_served_users,
     is_banned_user,
     is_on_off,
 )
 from AarumiMusic.utils.decorators.language import LanguageStart
 from AarumiMusic.utils.formatters import get_readable_time
-from AarumiMusic.utils.inline import first_page, private_panel, start_panel
+from AarumiMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
-STICKER = [
-        "CAACAgUAAxkBAAEBL29nho9ewF0e4aUP05Z_haRVCScpkgACngEAAgx_iVfmazE9xbmj_R4E",
-        "CAACAgUAAxkBAAEBL3Bnho9edQ3env6-ayR5E4vfS_fx-wACQAEAAg4uiVdelMPu_8BiQR4E"
-        "CAACAgUAAxkBAAEBL3Fnho9fSdZgtn18rBHkPVBbVB55fwACtgEAAq-6iVclVowlEMgs9h4E",
-        "CAACAgUAAxkBAAEBL3Jnho9hn2Zjjk5xf23hc2ZLNRDw7gACjQEAAlgzyFfn6fcRs_J9YR4E",
-        "CAACAgUAAxkBAAEBL3Nnho9hMDuEtzHd3f8YWizPP-aH4gAC8AEAAjREiVdB_ysq-ctClR4E",
-        "CAACAgUAAxkBAAEBL3Rnho9jbzfA4Ljox-gdwvWekZY9vQACcwIAAl6TqVWFNPy_WS5bax4E",
-        "CAACAgUAAxkBAAEBL3Vnho9ouf4Oig3_neT1mr11BanP8gACfQEAAu14sVcpw-MZJT2S6h4E",
-        "CAACAgUAAxkBAAEBL3Znho9pdvbWndWLOXIdZorYaP1VPgACrAEAAkuOwVdzqZh19Ol7RB4E",
-        "CAACAgUAAxkBAAEBL3dnho9wj60On5cbNkHj5lgzPO0ZbQACQwIAAp3CsVUV8b4slQg-fh4E",
-        "CAACAgUAAxkBAAEBL3hnho9x6apcO7Rw-2F4KCQD6NhzcwACgwIAApbkuFVY51aPcyVZfB4E",
+SHASHANK_VD = ["https://telegra.ph/file/89c5023101b65f21fb401.mp4",
+          "https://telegra.ph/file/bbc914cce6cce7f607641.mp4",
+          "https://telegra.ph/file/abc578ecc222d28a861ba.mp4",
+          "https://telegra.ph/file/065f40352707e9b5b7c15.mp4",
+          "https://telegra.ph/file/52ceaf02eae7eed6c9fff.mp4",
+          "https://telegra.ph/file/299108f6ac08f4e65e47a.mp4",
+          "https://telegra.ph/file/7a4e08bd04d628de71fc1.mp4",
+          "https://telegra.ph/file/0ad8b932fe5f7684f941c.mp4",
+          "https://telegra.ph/file/95ebe2065cfb1ac324a1c.mp4",
+          "https://telegra.ph/file/98cf22ccb987f9fedac5e.mp4",
+          "https://telegra.ph/file/f1b1754fc9d01998f24df.mp4",
+          "https://telegra.ph/file/421ee22ed492a7b8ce101.mp4"]
+
+SHASHANK_PH = [
+    "https://files.catbox.moe/jrupn9.jpg",
+    "https://files.catbox.moe/5z141p.jpg",
+    "https://files.catbox.moe/fnl0h7.jpg",
+    "https://files.catbox.moe/1lz1go.jpg",
+    "https://files.catbox.moe/avackl.jpg",
+    "https://files.catbox.moe/1yrzwz.jpg",
+    "https://files.catbox.moe/6y22qw.jpg",
+    "https://files.catbox.moe/gnnsf2.jpg",
+    "https://files.catbox.moe/ss6r60.jpg",
+    "https://files.catbox.moe/yuob18.jpg",
+    "https://files.catbox.moe/i9xrrp.jpg",
+    "https://files.catbox.moe/a9tx8f.jpg"
+    "https://files.catbox.moe/wlt26x.jpg",
+    "https://files.catbox.moe/c1lylh.jpg",
+    "https://files.catbox.moe/82eymp.jpg",
 ]
 
-START_IMG_URL = [
-    "https://files.catbox.moe/u7iwi5.jpg",
-    "https://files.catbox.moe/rkskww.jpg",
-    "https://files.catbox.moe/uz810n.jpg",
-    "https://files.catbox.moe/vzl4xb.jpg",
-    "https://files.catbox.moe/j2zdsu.jpg",
-    "https://files.catbox.moe/nlweex.jpg",
-    "https://files.catbox.moe/pp4god.jpg",
-    "https://files.catbox.moe/jslln0.jpg",
-    "https://files.catbox.moe/fqmdff.jpg",
-    "https://files.catbox.moe/eushjw.jpg",
-    "https://files.catbox.moe/39fg3k.jpg",
+STICKERS = [  
+    "CAACAgUAAxkBAAEBdnpm-CKSe_WqOslpePn3ECfb_RaE9wACsRIAAk3CoVcRpQ6OPeMXSh4E",
+    "CAACAgUAAxkBAAEBdnxm-CKSAvkd77cIkLVi22i5KetRwAACjBIAAmiswVfmvH70I9yO1B4E",
+    "CAACAgUAAxkBAAEBdn1m-CKTitpex6wRSb7XepTBL1m7hQACGBMAAgj0wVc1DtFNJKC_Ih4E",
+    "CAACAgUAAxkBAAEBdn5m-CKUm8kE8yW1wfVn93uPBvE3nwACoxMAArRPwFeUVWErgkaFyR4E",
+    "CAACAgUAAxkBAAEBdn9m-CKVC9XUkmuOW6b1gIL0Teg6HgACyREAAoz_wFeQweR6NJm6Yh4E",
+    "CAACAgUAAxkBAAEBdntm-CKSYkLSIPrQAiOxMeBfyZpGegACfRAAAlMQwVdx2UGzjxf3CR4E",
 ]
 
-VALID_EMOJII = ["🔥", "💋", "🥺", "😒", "💖",
-                "💘", "💕", "✨", "🥰", "🍌", "💔",
-                "😓", "🫧"]
+async def delete_sticker_after_delay(message, delay):
+    await asyncio.sleep(delay)
+    await message.delete()
 
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
-    random_emoji = random.choice(VALID_EMOJII)  # Pick a valid emoji
-    try:
-        await message.react(random_emoji)
-    except Exception as e:
-        print(f"Error reacting with emoji: {e}")
     if len(message.text.split()) > 1:
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
-            keyboard = first_page(_)
-            return await message.reply_photo(
-                photo=config.START_IMG_URL,
+            keyboard = help_pannel(_)
+            await message.reply_video(
+                random.choice(SHASHANK_VD),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
-        if name[0:3] == "sud":
+        elif name[0:3] == "sud":
             await sudoers_list(client=client, message=message, _=_)
             if await is_on_off(2):
-                return await app.send_message(
+                await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                    text=f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>๏ ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>๏ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
-            return
-        if name[0:3] == "inf":
+        elif name[0:3] == "inf":
             m = await message.reply_text("🔎")
-            query = (str(name)).replace("info_", "", 1)
+            query = str(name).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
             for result in (await results.next())["result"]:
@@ -107,53 +117,44 @@ async def start_pm(client, message: Message, _):
                 ]
             )
             await m.delete()
-            await app.send_photo(
+            await app.send_video(
                 chat_id=message.chat.id,
-                photo=thumbnail,
+                video=thumbnail,
                 caption=searched_text,
                 reply_markup=key,
             )
             if await is_on_off(2):
-                return await app.send_message(
+                await app.send_message(
                     chat_id=config.LOGGER_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                    text=f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>๏ ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>๏ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
     else:
         out = private_panel(_)
-
-        # First start_2 send photo with caption
-        sticker_msg = await message.reply_sticker(
-                sticker=random.choice(STICKER),  # Random sticker from the list
-            )
-        await asyncio.sleep(0.5)  # the sticker will be deleted after 0.5 seconds
-        await sticker_msg.delete()  # delete the sticker message
-
-        await message.reply_photo(
-            photo=random.choice(START_IMG_URL),
-            caption=_["start_1"].format(message.from_user.mention), parse_mode=ParseMode.HTML,)
-
-        await message.reply_text(text=_["start_2"].format(app.mention), parse_mode=ParseMode.HTML, reply_markup=InlineKeyboardMarkup(out),)
-
-        # Agar logging on hai, to message bhejo
+        served_chats = len(await get_served_chats())
+        served_users = len(await get_served_users())
+        UP, CPU, RAM, DISK = await bot_sys_stats()
+        await message.reply_video(
+            random.choice(SHASHANK_VD),
+            caption=_["start_2"].format(message.from_user.mention, app.mention, UP, DISK, CPU, RAM, served_users, served_chats),
+            reply_markup=InlineKeyboardMarkup(out),
+        )
         if await is_on_off(2):
-            return await app.send_message(
+            await app.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
+                text=f"❖ {message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>๏ ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>๏ ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
             )
-
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
-    await message.reply_photo(
-        photo=config.START_IMG_URL,
-        caption=_["start_0"].format(app.mention, get_readable_time(uptime)),
+    await message.reply_video(
+        random.choice(SHASHANK_VD),
+        caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
     return await add_served_chat(message.chat.id)
-
 
 @app.on_message(filters.new_chat_members, group=-1)
 async def welcome(client, message: Message):
@@ -183,9 +184,9 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_photo(
-                    photo=config.START_IMG_URL,
+                    random.choice(SHASHANK_PH),
                     caption=_["start_3"].format(
-                        message.from_user.first_name,
+                        message.from_user.mention,
                         app.mention,
                         message.chat.title,
                         app.mention,
