772673ab8e325f16bb2b07cec845a703faa82e11
Aarumi Bots <itzshivanshdevs@gmail.com>
2025-06-04 16:20:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", ""
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AarumiBots)
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AarumiBots")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AarumiChat1")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
