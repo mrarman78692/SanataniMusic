9906a60aa5632abd3ef83d5469e0b91ad03b668a
Aarumi Bots <itzshivanshdevs@gmail.com>
2025-06-05 13:18:44 +0530

    Update ping.py

--- AarumiMusic/plugins/tools/ping.py
+++ AarumiMusic/plugins/tools/ping.py
@@ -27,7 +27,7 @@ async def ping_com(client, message: Message, _):
     response = await message.reply_photo(
         photo=random.choice(SHASHANK_PIC),
         has_spoiler=True,
- caption=_["ping_1"].format(app.mention),
+       caption=_["ping_1"].format(app.mention),
     )
     pytgping = await Aarumi.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
