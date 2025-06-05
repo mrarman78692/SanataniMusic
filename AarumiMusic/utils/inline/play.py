b060a4bec3b52820a4ad3beb456055b73855310b
Aarumi Bots <itzshivanshdevs@gmail.com>
2025-06-05 07:12:48 +0530

    Update play.py

--- AarumiMusic/utils/inline/play.py
+++ AarumiMusic/utils/inline/play.py
@@ -61,6 +61,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton("ʏᴛ-ᴀᴘɪ", callback_data="bot_info_data"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
         ],
         [
