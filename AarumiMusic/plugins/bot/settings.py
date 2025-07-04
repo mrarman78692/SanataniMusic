dfc89256c1cfd02aeb22f510082d1d312eb37226
Aɴᴏɴʏᴍᴏᴜs <anonymousxd626w87@outlook.com>
2025-07-05 04:32:20 +0530

    Update settings.py

--- AarumiMusic/plugins/bot/settings.py
+++ AarumiMusic/plugins/bot/settings.py
@@ -42,14 +42,14 @@ from AarumiMusic.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 SHASHANK_PIC = [
-    "https://files.catbox.moe/t6485t.jpg",
-    "https://files.catbox.moe/4osoc3.jpg",
-    "https://files.catbox.moe/t6485t.jpg",
-    "https://files.catbox.moe/5e18lv.jpg",
-    "https://files.catbox.moe/bupvsx.jpg",
-    "https://files.catbox.moe/w7f2wa.jpg",
-    "https://files.catbox.moe/2b2dam.jpg",
-    "https://files.catbox.moe/qity19.jpg"
+    "https://files.catbox.moe/4rpo7o.jpg",
+    "https://files.catbox.moe/hgvcz6.jpg",
+    "https://files.catbox.moe/b9vscc.jpg",
+    "https://files.catbox.moe/d5gvwa.jpg",
+    "https://files.catbox.moe/n3dh8g.jpg",
+    "https://files.catbox.moe/vwe6s7.jpg",
+    "https://files.catbox.moe/90ad4u.jpg",
+    "https://files.catbox.moe/yzd87s.jpg"
 ]
 
 
